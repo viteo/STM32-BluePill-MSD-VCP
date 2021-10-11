ec99941596fdc4f066b21e68f67587c3f5f74d0e
viteo <simonenko@rtp>
2021-10-11 12:01:00 +0300

    Composite MSC-CDC folder

--- lib/USB_Composite_MSC-CDC/virtualComPort.h
+++ lib/USB_Composite_MSC-CDC/virtualComPort.h
@@ -5,8 +5,8 @@
  *      Author: v.simonenko
  */
 
-#ifndef USB_VIRTUALCOMPORT_VIRTUALCOMPORT_H_
-#define USB_VIRTUALCOMPORT_VIRTUALCOMPORT_H_
+#ifndef USB_VIRTUALCOMPORT_H_
+#define USB_VIRTUALCOMPORT_H_
 
 #include <stdint.h>
 
@@ -15,4 +15,4 @@ void VCP_ProcessRxBuff(uint8_t *data_buffer, uint8_t Nb_bytes);
 void VCP_SendTxBufPacketToUsb(void);
 void Handle_USBAsynchXfer(void);
 
-#endif /* USB_VIRTUALCOMPORT_VIRTUALCOMPORT_H_ */
+#endif /* USB_VIRTUALCOMPORT_H_ */
